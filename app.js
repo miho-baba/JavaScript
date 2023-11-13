186f6fd9d8de08c6f4963cc40ce9387c39be098d
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-11-13 03:56:21 +0000

    JavaScriptの基本文法の学習

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-alert('Hello World');
+//let hello = 'Hello World';
+//alert(hello);
+
+//【変数に代入できるコードの例】
+
+//整数を代入する
+//let int = 1;
+
+//負数を代入する
+//let int2 = -10;
+
+//少数点を代入する
+//let float1 = 3.14
+
+//文字列を代入する
+//let str1 = 'JaveScriptを覚えよう';
+
+//【四則演算の書き方】
+
+//足し算
+alert(4 + 3);
+
+//引き算
+alert(8 - 5);
+
+//掛け算
+alert(2 * 6);
+
+//割り算
+alert(10/2);
+
