3beb879e3d991923f6c65b7de0be34580d70597f
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-11-16 12:23:33 +0000

    JavaScriptの変数宣言:再宣言について

---
+++ practice-variable/sample.js
@@ -0,0 +1,19 @@
+// varによる再宣言
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+// letでは再宣言はできない
+
+// constによる再宣言
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+// constも再宣言はできないためletの時と同様のエラーが発生する
