0e87cb61c318f222be5e83293ba33afdb28982e0
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-11-18 08:28:11 +0000

    JavaScriptの変数宣言:再代入について

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -17,3 +17,13 @@
 // const nickname = "ichiro"
 // console.log(nickname)
 // constも再宣言はできないためletの時と同様のエラーが発生する
+
+// 再代入について
+// ※constでは再代入はできない
+
+// varによる再代入
+var nickname = "taro"
+console.log(nickname)
+nickname = "Jiro"
+console.log(nickname)
+
