c27a58722b0371fa1c97f6eda2c1c245b6669468
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-11-24 13:36:28 +0000

    JavaScript:引数と戻り値の記述について

--- practice-variable/Folder2/app.js
+++ practice-variable/Folder2/app.js
@@ -7,7 +7,13 @@
 // 引数と戻り値
 // ※入力する値を引数、出力される値を戻り値という
 // 下記のように記述する
-function 関数名(引数){
-  処理
-  return 戻り値;
+// function 関数名(引数){
+  // 処理
+  // return 戻り値;
+// }
+
+// 簡単な関数を記述してみる
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
 }
