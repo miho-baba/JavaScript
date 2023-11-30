c3fa2c070fe8c0d10547498a93feaaaa7a96dcdc
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-11-30 14:32:13 +0000

    複数の関数を定義の続き

--- practice-variable/Folder2/app.js
+++ practice-variable/Folder2/app.js
@@ -43,19 +43,43 @@
 // 複数の関数を定義する
 // 下記の内容は、入力した値を処理する関数を作成した物である
 
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んで下さい。');
-alert('あなたの選んだ手は、' + user_hand + 'です。')
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んで下さい。');
+// alert('あなたの選んだ手は、' + user_hand + 'です。')
 // ※下記は、上記の内容に相手のじゃんけんの手をランダムに選択する関数を追加した物である
+// function getJShand(){
+  // let js_hand_num = Math.floor( Math.random() ＊３ );
+
+  // if(js_hand_num == 0){
+    // js_hand = "グー";
+  // } else if(js_hand_num == 1){
+    // js_hand = "チョキ";
+  // } else if(js_hand_num == 2){
+    // js_hand = "パー";
+  // }
+
+  // return js_hand;
+// }
+
+// ※上記の内容を応用し、入力されたグー・チョキ・パーと、javaScriptで生成した
+// ランダムなじゃんけんの手を比べ、勝ち・負け・引き分けの結果を表示させる。
+// 下記の内容がその記述です。
+
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んで下さい。')
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
+// ユーザの手とjavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
+// 結果を表示する
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+// ランダムでじゃんけんの手を作成する関数
 function getJShand(){
-  let js_hand_num = Math.floor( Math.random() ＊３ );
+  let js_hand_num = Math.floor( Math.rabdom() ＊ 3 );
+  let hand_name;
 
-  if(js_hand_num == 0){
-    js_hand = "グー";
-  } else if(js_hand_num == 1){
-    js_hand = "チョキ";
-  } else if(js_hand_num == 2){
-    js_hand = "パー";
-  }
-
-  return js_hand;
+  if(js)
+  
+  
+  
 }
+
