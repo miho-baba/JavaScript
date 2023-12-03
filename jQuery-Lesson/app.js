af8d6837b8f2f3c76815dd518a50c93a7dd37748
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-12-03 03:02:30 +0000

    jQueryについて

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,15 @@
+// $(document).ready(function () {
+  // $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// jQueryの書き方
+// 1パターン
 $(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+  // jQueryプログラムの内容
 });
+// 2パターン(省略形)
+$(function(){
+  // jQueryプログラムの内容
+});
+// jQueryの基本書式は、以下のようになる
+$( セレクタ ).メソッド ( パラメータ );
