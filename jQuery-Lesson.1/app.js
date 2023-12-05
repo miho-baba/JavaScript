08deaa77a0404c70e6f2f55ee9b35e61bcc27089
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-12-05 06:09:59 +0000

    jQuery:アニメーション非表示の要素をjQueryで表示させる

--- jQuery-Lesson.1/app.js
+++ jQuery-Lesson.1/app.js
@@ -3,7 +3,13 @@ $(function(){
   $('.box1').slideDown();
 });
 */
-/* 下から上へスライドさせる記述 */
+/* 下から上へスライドさせる記述
 $(function(){
   $('.box1').slideUp();
+});
+*/
+/* 非表示の要素をjQueryで表示させる記述 */
+$(function(){
+  $('.box1').show();
+  $('.box1').css({'background-color': '#0000FF'});
 });
