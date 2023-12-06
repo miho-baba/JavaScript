aad66a803dbc4228290c86e757092c4f769ddcb0
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-12-06 05:30:49 +0000

    表示されている要素をjQueryで非表示にする

--- jQuery-Lesson.1/app.js
+++ jQuery-Lesson.1/app.js
@@ -8,8 +8,13 @@ $(function(){
   $('.box1').slideUp();
 });
 */
-/* 非表示の要素をjQueryで表示させる記述 */
+/* 非表示の要素をjQueryで表示させる記述
 $(function(){
   $('.box1').show();
   $('.box1').css({'background-color': '#0000FF'});
+}); */
+
+/* 表示されている要素をjQueryで非表示にする記述 */
+$(function(){
+  $('.box1').hide();
 });
