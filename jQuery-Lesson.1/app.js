6fb7053d5c671704f8e81d8ed7422a5944165939
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-12-07 10:51:46 +0000

    jQuery:確認問題

--- jQuery-Lesson.1/app.js
+++ jQuery-Lesson.1/app.js
@@ -23,3 +23,12 @@ $(function(){
 上記のプログラムを改良して、以下のような動作を実現させる。
 ①要素を上から下へスライドさせた後、赤色の正方形を青色の長方形(幅200px、高さ100px)に変更する。
 ②①の後、下から上へスライドさせる */
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
+});
